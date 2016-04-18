ddb9ef0f79d2741c32a2c0b131b6a2e85e19446c
Cassia Almeida <lisa.shiphrah@gmail.com>
2016-04-18 08:21:37 -0300

    Update ConversaoMassa.cs

--- ConversorPhone/Helpers/ConversaoMassa.cs
+++ ConversorPhone/Helpers/ConversaoMassa.cs
@@ -8,7 +8,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
-
+  
 namespace MeasuresConverter.Helpers
 {
     /// <summary>
